0f47fb5bad258632e6c75c53e425656c66ec3ad6
Temilola <temz4rill@gmail.com>
2021-07-11 10:57:46 -0400

    Implemented Accessibility Features

--- app/src/main/java/com/udacity/asteroidradar/util/BindingAdapters.kt
+++ app/src/main/java/com/udacity/asteroidradar/util/BindingAdapters.kt
@@ -15,19 +15,33 @@ import com.udacity.asteroidradar.util.AsyncOperationState
 
 @BindingAdapter("statusIcon")
 fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
+    val context = imageView.context
     if (isHazardous) {
         imageView.setImageResource(R.drawable.ic_status_potentially_hazardous)
+        imageView.contentDescription = context.getString(
+            R.string.potentially_hazardous_asteroid_image
+        )
     } else {
         imageView.setImageResource(R.drawable.ic_status_normal)
+        imageView.contentDescription = context.getString(
+            R.string.not_hazardous_asteroid_image
+        )
     }
 }
 
 @BindingAdapter("asteroidStatusImage")
 fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
+    val context = imageView.context
     if (isHazardous) {
         imageView.setImageResource(R.drawable.asteroid_hazardous)
+        imageView.contentDescription = context.getString(
+            R.string.potentially_hazardous_asteroid_image
+        )
     } else {
         imageView.setImageResource(R.drawable.asteroid_safe)
+        imageView.contentDescription = context.getString(
+            R.string.not_hazardous_asteroid_image
+        )
     }
 }
 
@@ -35,18 +49,23 @@ fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
 fun bindTextViewToAstronomicalUnit(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.astronomical_unit_format), number)
+    textView.contentDescription =
+        String.format(context.getString(R.string.astronomical_unit_format), number)
 }
 
 @BindingAdapter("kmUnitText")
 fun bindTextViewToKmUnit(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.km_unit_format), number)
+    textView.contentDescription = String.format(context.getString(R.string.km_unit_format), number)
 }
 
 @BindingAdapter("velocityText")
 fun bindTextViewToDisplayVelocity(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.km_s_unit_format), number)
+    textView.contentDescription =
+        String.format(context.getString(R.string.km_s_unit_format), number)
 }
 
 @BindingAdapter("asteroidData")
@@ -60,10 +79,13 @@ fun bindRecyclerViewToDisplayAsteroidList(recyclerView: RecyclerView, data: List
 
 @BindingAdapter("imageUrl")
 fun setImageUrl(imageView: ImageView, imageObj: PictureOfDay?) {
+    val context = imageView.context
     if (imageObj == null) return
     Picasso.with(imageView.context).load(imageObj.url).into(imageView)
-    imageView.contentDescription = imageView.context.getString(
-        R.string.nasa_picture_of_day_content_description_format, imageObj.title
+    imageView.contentDescription = String.format(
+        context.getString(
+            R.string.nasa_picture_of_day_content_description_format
+        ), imageObj.title
     )
 }
 
